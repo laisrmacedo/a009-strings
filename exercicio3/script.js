0a17134b181c78599107c33dfc6b0514606cb8e9
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-08-31 19:18:34 +0200

    Resolucao exercicio 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,14 @@
-//Crie a const para a frase aqui
+//a
+const frase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"`
+console.log(frase)
+
+//b 
+const novaFrase = frase.replace("verde", "rosa").replace("azul", "laranja")
+console.log(novaFrase)
+
+//c Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+console.log(novaFrase.includes("verde"))
+console.log(novaFrase.includes("laranja"))
+
+
+
