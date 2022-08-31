7cfca9cc2f7c4dcd6276dc3caa555b8aa402e877
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-08-31 19:03:22 +0200

    Resolucao exercicio 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,16 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+//a
+console.log(minhaString.trim())
+
+//b
+console.log(`
+Quantidade de caracteres da string:
+Antes: ${minhaString.length}
+Depois: ${minhaString.trim().length}
+`)
+
+//c
+console.log(`
+    ${minhaString.replaceAll("________", "“sticioso”")}
+`)
