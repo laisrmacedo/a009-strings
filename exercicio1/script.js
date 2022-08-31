7324bf2f5be522ee4b0e8826e53966a3ce1f1c6c
laisrmacedo <laisrodriguesmacedo@gmail.com>
2022-08-31 18:56:14 +0200

    Resolucao exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,12 @@
+let nome = prompt("Digite seu nome")
+let comida1 = prompt("Primeira comida favorita")
+let comida2 = prompt("Segunda comida favorita")
+let comida3 = prompt("Terceira comida favorita")
+
+console.log(`
+    Estas são as comidas favoritas de ${nome}:
+    - ${comida1}
+    - ${comida2}
+    - ${comida3}
+`)
+
